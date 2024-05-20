85bfa9611a0b10cdca1d578ff3eaa3843eb92230
Cyrus Ducut <cyrus.ducut@fieldforce4.com>
2024-05-20 10:18:21 +0800

    add toggle. merge landing page and chart

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,44 @@
+// TODO: use pixabay api
+import backgroundImage from '../images/background.png';
+import backgroundImage2 from '../images/city.png';
+
 const elQuote = document.querySelector('.quote');
 const elAuthor = document.querySelector('.quote-author');
-elQuote.innerHTML = '';
-elAuthor.innerHTML = '';
 
+const btnToday = document.querySelector('#btn-today');
+const btnFiveDays = document.querySelector('#btn-five-days');
+
+const elTodayView = document.querySelector('.today-view');
+const elFiveDayView = document.querySelector('.five-day-view');
+
+btnToday.addEventListener('click', () => toggleView());
+btnFiveDays.addEventListener('click', () => toggleView());
+
+let toggled = false;
+function toggleView() {
+  elTodayView.classList.toggle('visually-hidden');
+  elFiveDayView.classList.toggle('visually-hidden');
+  changeBackground(toggled ? backgroundImage2 : backgroundImage);
+  resetElements();
+  typeText();
+}
+
+function resetElements() {
+  index = 0;
+  elQuote.innerHTML = '';
+  elAuthor.innerHTML = '';
+}
+
+function changeBackground(newBg) {
+  document.body.style.backgroundImage = `linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0) 70%,
+      rgba(0, 0, 0, 0.9)
+    ), url(${newBg})`;
+  toggled = !toggled;
+}
+
+// TODO: use API for random quotes
 const text =
   "Who cares about the clouds when we're together? Just sing a song and bring the sunny weather.";
 const author = 'Dale Evans';
@@ -21,5 +57,6 @@ function typeText() {
 
 // Start typing the text when the page loads
 window.onload = function () {
+  resetElements();
   typeText();
 };
