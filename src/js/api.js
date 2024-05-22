d249eadef5687fadee7eaf2d155f32193728fade
Cy <62021926+cyan0511@users.noreply.github.com>
2024-05-22 21:31:48 +0800

    Create api.js

---
+++ src/js/api.js
@@ -0,0 +1,5 @@
+export const WEATHER_API_KEY = 'bb62771caae00d80a1cdcf2a46703e53';
+export const PIXABAY_API_KEY = '43554291-045e76b825ecd08fdbf56f121';
+
+export const WEATHER_API_ENDPOINT = 'https://pixabay.com/api/';
+export const PIXABAY_API_ENDPOINT = 'http://api.openweathermap.org/';
