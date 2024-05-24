cc25cfa79e5382fb193820ca78c8e64c8c79ab52
Cyrus Ducut <cyrus.ducut@fieldforce4.com>
2024-05-25 02:31:12 +0800

    dynamic image loading. get current location.

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,6 @@
 export const WEATHER_API_KEY = 'bb62771caae00d80a1cdcf2a46703e53';
 export const PIXABAY_API_KEY = '43554291-045e76b825ecd08fdbf56f121';
 
-export const WEATHER_API_ENDPOINT = 'https://pixabay.com/api/';
-export const PIXABAY_API_ENDPOINT = 'http://api.openweathermap.org/';
+export const PIXABAY_API_ENDPOINT = 'https://pixabay.com/api';
+export const WEATHER_API_ENDPOINT = 'https://api.openweathermap.org';
+export const WEATHER_ICON_ENDPOINT = 'https://openweathermap.org';
