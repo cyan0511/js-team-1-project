49fd95f35b3df7d0cf27371488887fd9654fa1f9
Cyrus Ducut <cyrus.ducut@fieldforce4.com>
2024-05-26 00:52:24 +0800

    change pixabay request per_page to 80

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -15,7 +15,7 @@ export async function searchImage(cityName) {
 }
 
 export async function searchRandomImage() {
-  const url = `${PIXABAY_API_ENDPOINT}/?image_type=photo&orientation=horizontal&category=places&q=sunset&page=1&per_page=10&key=${PIXABAY_API_KEY}`;
+  const url = `${PIXABAY_API_ENDPOINT}/?image_type=photo&orientation=horizontal&category=places&q=sunset&per_page=80&key=${PIXABAY_API_KEY}`;
   const response = await axios.get(url);
   if (response.status !== 200) throw new Error('Failed to fetch data.');
   const hits = response.data.hits;
