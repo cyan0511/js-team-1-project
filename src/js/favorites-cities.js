8fa57c4b47d6aa90b22aee7a3dd898e68ef0800a
Gerald-Ivan <ardiente.gerald@gmail.com>
2024-05-26 21:48:05 +0800

    Update favorites-cities.js

--- src/js/favorites-cities.js
+++ src/js/favorites-cities.js
@@ -1,27 +1,27 @@
 import { Notify } from "notiflix";
 
-const favorite_btn = document.querySelector('.btn-favourite');
-const favorite_icon = document.querySelector('.fovourite-icon');
-const search_data = document.querySelector('#search-input');
-const favorite_city_list = document.querySelector('.favorite_city-list');
+const favoriteBtn = document.querySelector('.btn-favourite');
+const favoriteIcon = document.querySelector('.fovourite-icon');
+const searchData = document.querySelector('#search-input');
+const favoriteCityList = document.querySelector('.favorite_city-list');
 const favorite_list_close = document.querySelector('.favorite_list-close');
 
-favorite_btn.addEventListener('click', addToFavorite);
-favorite_icon.addEventListener('click', addToFavorite);
+favoriteBtn.addEventListener('click', addToFavorite);
+favoriteIcon.addEventListener('click', addToFavorite);
 
 
 function addToFavorite (){
-    if (search_data.value === ""){
+    if (searchData.value === ""){
         Notify.info("Please enter a city!");
         return;
     }else{
         let search_array = JSON.parse(localStorage.getItem('city')) || [];
-        if(search_array.map(item => item.toUpperCase()).includes(search_data.value.toUpperCase())){
+        if(search_array.map(item => item.toUpperCase()).includes(searchData.value.toUpperCase())){
             return Notify.info("Already added to favorites!");
         }else{
-        search_array.push(search_data.value);
+        search_array.push(searchData.value);
         localStorage.setItem('city', JSON.stringify(search_array));
-        search_data.value = '';
+        searchData.value = '';
         updateCityList(); 
         Notify.info("Added to favorites!");
         }
@@ -29,7 +29,7 @@ function addToFavorite (){
     }}
 function updateCityList(){
     const storedCities = JSON.parse(localStorage.getItem('city')) || [];
-    favorite_city_list.innerHTML = ''; 
+    favoriteCityList.innerHTML = ''; 
     storedCities.forEach(city =>{
             const listItem = `
             <li class="favorite-list_item">
@@ -37,7 +37,7 @@ function updateCityList(){
             <button class="favorite_list-close" type="button"></button>
             </li>
         `;
-        favorite_city_list.insertAdjacentHTML('beforeend', listItem);
+        favoriteCityList.insertAdjacentHTML('beforeend', listItem);
     })
     const removeButtons = document.querySelectorAll('.favorite_list-close');
     removeButtons.forEach(button => {
