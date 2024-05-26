d02fa720af04ecb0b29d401853837f04e738cbaa
Jummelmarijuan <142652721+Jummelmarijuan@users.noreply.github.com>
2024-05-26 22:32:40 +0900

    update

--- src/js/hide-show.js
+++ src/js/hide-show.js
@@ -1,15 +1,13 @@
-//Hide and Show
-
 const divContainer = document.querySelector('#more-info-data');
-let isClicked = true;
 
+    let isClicked = true;
 
-let hideShow = function () {
-    if(isClicked){
-    divContainer.style.display = "block";
-    isClicked = false;    
-    } else {
-        divContainer.style.display = 'none';
-        isClicked = true;
+    function hideShow(event) {
+        if (isClicked) {
+            divContainer.style.display = 'block';
+            isClicked = false;
+        } else {
+            divContainer.style.display = 'none';
+            isClicked = true;
+        }
     }
-}
