aa3328cbeabbe3502934b7a57d4c2cea00427eec
Cyrus Ducut <cyrus.ducut@fieldforce4.com>
2024-05-28 03:11:09 +0800

    render five day view and more info

--- src/js/five-days.js
+++ src/js/five-days.js
@@ -1,239 +1,8 @@
-import axios from 'axios';
 import moment from 'moment/moment';
-
-import {
-  WEATHER_API_ENDPOINT,
-  WEATHER_API_MORE_INFO_ENDPOINT,
-  WEATHER_API_OPTIONS,
-  GEOCODING_API_ENDPOINT,
-  GEOCODING_API_OPTIONS,
-} from './api';
+import { WEATHER_ICON_ENDPOINT } from './api';
 
 const searchInputEl = document.getElementById('search-input');
 const searchFormEl = document.getElementById('search-form');
-
-// Main function for five days view and more info
-async function handleFiveDaysViewAndMoreInfo(e) {
-  e.preventDefault();
-  const fivedaysViewListEl = document.querySelector('.five-days-view-list');
-  const moreInfoListEl = document.querySelector('.more-info-list');
-
-  // remove previous search forcast
-  if (fivedaysViewListEl !== null) {
-    fivedaysViewListEl.remove();
-  }
-
-  if (moreInfoListEl !== null) {
-    moreInfoListEl.remove();
-  }
-
-  try {
-    let res = await getSearchCityCoordinates();
-
-    if (res.data.length > 0) {
-      const moreInfoEl = document.querySelector('.more-info-container');
-
-      // Set city coordinates
-      WEATHER_API_OPTIONS.params.lat = res.data[0].lat;
-      WEATHER_API_OPTIONS.params.lon = res.data[0].lon;
-
-      // console.log(res.data[0].lat + '--' + res.data[0].lon);
-
-      res = await getWeatherFiveDays();
-      // console.log(res);
-
-      // Set five days view
-      const fiveDaysViewMarkup = getFiveDaysViewMarkup(res);
-      moreInfoEl.insertAdjacentHTML('afterbegin', fiveDaysViewMarkup);
-
-      // Set event listener for for more info
-      const moreInfoLink = document.querySelectorAll('.more-info-link');
-      moreInfoLink.forEach(link => {
-        link.addEventListener('click', toggleMoreInfoView);
-      });
-
-      // Set more info view
-      const moreInfoMarkup = getMoreInfoMarkup(res);
-      moreInfoEl.insertAdjacentHTML('beforeend', moreInfoMarkup);
-    } else {
-      alert('NO LOCATION');
-    }
-  } catch (error) {
-    console.log(1 + error);
-  }
-}
-
-// Convert city name to latitude and longitude coordinates from API
-async function getSearchCityCoordinates() {
-  if (searchInputEl.value === '') return;
-  GEOCODING_API_OPTIONS.params.q = searchInputEl.value.trim();
-
-  try {
-    return await axios.get(
-      WEATHER_API_ENDPOINT + GEOCODING_API_ENDPOINT,
-      GEOCODING_API_OPTIONS
-    );
-  } catch (e) {
-    console.log(2 + error);
-  }
-}
-
-// Get five days weather forecasts from API
-async function getWeatherFiveDays() {
-  try {
-    return await axios.get(
-      WEATHER_API_ENDPOINT + WEATHER_API_MORE_INFO_ENDPOINT,
-      WEATHER_API_OPTIONS
-    );
-  } catch (e) {
-    throw e;
-  }
-}
-
-// Logic and markup for Five Days View
-function getFiveDaysViewMarkup(res) {
-  let markup = '<ul class="five-days-view-list">';
-
-  // get five days dates
-  const fiveDays = res.data.list
-    .map(({ dt_txt }) => {
-      return dt_txt.split(' ')[0];
-    })
-    .filter((date, index, array) => array.indexOf(date) === index);
-
-  // loop through the five days and get the weather forcasts for each day
-  fiveDays.forEach(day => {
-    // console.log(day);
-
-    const formattedDate = moment(day).format('dddd-DD-MMM-LT').split('-');
-    const weatherToday = {};
-
-    weatherToday.fullDate = day;
-    weatherToday.dayOfTheWeek = formattedDate[0];
-    weatherToday.day = formattedDate[1];
-    weatherToday.month = formattedDate[2];
-
-    // get the forcasts of this day
-    const forcastsOfTheDayArray = Object.groupBy(res.data.list, ({ dt_txt }) =>
-      dt_txt.includes(day)
-    )['true'];
-
-    // console.log(forcastOfTheDayArray);
-
-    // get the min and max temperature of this day among the temperatures of the forecasts
-    const temperatureArray = forcastsOfTheDayArray.flatMap(({ main }) => {
-      return [main.temp_min, main.temp_max];
-    });
-    // console.log(temperatureArray);
-    weatherToday.min = Math.min(...temperatureArray);
-    weatherToday.max = Math.max(...temperatureArray);
-
-    // find the most occurrences of weather icons in the forecast to be declared as weather of this day
-    const weatherArray = forcastsOfTheDayArray.map(({ weather }) => weather[0]);
-    // console.log(weatherArray);
-    weatherToday.weather = getWeatherMode(weatherArray);
-    // console.log(weatherToday);
-
-    markup += createFiveDaysMarkup(weatherToday);
-  });
-  markup += '</ul>';
-
-  return markup;
-}
-
-// Logic and markup for More Info
-function getMoreInfoMarkup(res) {
-  let moreInfoMarkup = '<ul class="more-info-list">';
-
-  moreInfoMarkup += res.data.list
-    .map(({ dt_txt, weather, rain, main, wind }) => {
-      const formattedDate = moment(dt_txt).format('dddd-DD-MMM-LT').split('-');
-
-      return `
-      <li class="more-info-item visually-hidden" data-date="${
-        dt_txt.split(' ')[0]
-      }">
-        <h3 class="time">${formattedDate[0]} ${formattedDate[1]} ${
-        formattedDate[2]
-      } ${formattedDate[3]}</h3>
-        <div class="weather-icon">
-          <img src=" https://openweathermap.org/img/w/${
-            weather[0].icon
-          }.png" alt="${weather[0].description}" title="${
-        weather[0].description
-      } - ${weather[0].id}" width="50"/>
-        </div>
-        <h2 class="temperature">${main.temp_min}°C</h2>
-        <ul class="measurement-list">
-          <li class="measurement-item">
-            <span class="icon">
-              ${
-                document.querySelector('.more-info-container .icon-barometer')
-                  .innerHTML
-              }
-            </span>
-            <span class="barometer">${
-              rain === null || rain === undefined
-                ? '0 '
-                : rain[Object.keys(rain)[0]]
-            } mm</span>
-          </li>
-          <li class="measurement-item">
-            <span class="icon">
-            ${
-              document.querySelector('.more-info-container .icon-humidity')
-                .innerHTML
-            }
-            </span>
-            <span class="humidity">${main.humidity}%</span>
-          </li>
-          <li class="measurement-item">
-            <span class="icon">
-            ${
-              document.querySelector('.more-info-container .icon-wind')
-                .innerHTML
-            }
-            </span>
-            <span class="wind">${wind.speed} m/s</span>
-          </li>
-        </ul>
-      </li>`;
-    })
-    .join('');
-
-  moreInfoMarkup += '</ul>';
-
-  return moreInfoMarkup;
-}
-
-// Markup for five days view
-function createFiveDaysMarkup(weatherToday) {
-  return `<li class="more-info-item">
-   <h3 class="time">${weatherToday.dayOfTheWeek}</h3>
-   <h2>${weatherToday.day} ${weatherToday.month}</h2>
-   <div class="weather-icon">
-     <img src=" https://openweathermap.org/img/w/${weatherToday.weather.icon}.png" alt="${weatherToday.weather.description}" title="${weatherToday.weather.description}" width="50"/>
-      </div>
-      <h2 class="temperature">${weatherToday.min}°C | ${weatherToday.max}°C</h2>
-      <a class="more-info-link" href="#" data-date="${weatherToday.fullDate}">More Info</a>
-    </li>`;
-}
-
-// Toggle more info view for each day
-function toggleMoreInfoView(e) {
-  e.preventDefault();
-
-  const moreInfoEl = document.querySelectorAll(
-    '.more-info-list .more-info-item'
-  );
-
-  moreInfoEl.forEach(info => {
-    info.classList.add('visually-hidden');
-    if (info.dataset.date === e.target.dataset.date) {
-      info.classList.remove('visually-hidden');
-    }
-  });
-}
 
 // Get the weather of the day with the most occurrences by comparing the weather icons
 function getWeatherMode(weatherArray) {
@@ -265,4 +34,150 @@ function getWeatherMode(weatherArray) {
   return modes[0];
 }
 
-searchFormEl.addEventListener('submit', handleFiveDaysViewAndMoreInfo);
+export function renderFiveDaysData(fiveDaysWeather) {
+  const formattedObject = fiveDaysWeather.list
+    .map(weather => moment(weather.dt_txt).format('yy-MM-DD'))
+    .filter((x, i, a) => a.indexOf(x) == i)
+    .map(date => ({
+      key: date,
+      list: fiveDaysWeather.list.filter(
+        w => moment(w.dt_txt).format('yy-MM-DD') === date
+      ),
+    }));
+
+  const data = formattedObject.map(obj => ({
+    ...obj,
+    day: moment(obj.key, 'yy-MM-DD').format('dddd'),
+    date: moment(obj.key, 'yy-MM-DD').format('DD MMM'),
+    min: Math.min(...obj.list.map(l => l.main.temp_min)),
+    max: Math.max(...obj.list.map(l => l.main.temp_max)),
+    icon: obj.list[0].weather[0].icon,
+    weatherName: obj.list[0].weather[0].main,
+  }));
+
+  if (data.length > 5) {
+    data.pop();
+  }
+
+  const cityElement = document.querySelector('.city-fiveday-view');
+  cityElement.textContent = `${fiveDaysWeather.city.name}, ${fiveDaysWeather.city.country}`;
+  const weatherWrapElement = document.querySelector('.weather-wrap');
+  const ul = `
+  <ul class="card-content">
+    ${Array.from(
+      data.map(
+        weather =>
+          `<li data-key="${weather.key}" class="card" id="card">
+                  <p id="day" class="day" tabindex="0">${weather.day}</p>
+                  <p class="fivedays-date">${weather.date}</p>
+                  <img src="${WEATHER_ICON_ENDPOINT}/img/wn/${
+            weather.icon
+          }@2x.png" alt="" class="fivedays-icon">
+                  <div class="temp-info">
+                    <div class="box">
+                      <p class="text-min">min</p>
+                      <p class="num-min"> ${Math.floor(weather.min)}°</p>
+                    </div>
+                    <div class="separator"></div>
+                    <div class="box">
+                      <p class="text-max">max</p>
+                      <p class="num-max"> ${Math.floor(weather.max)}°</p>
+                    </div>
+                  </div>
+                  <button data-key="${
+                    weather.key
+                  }" class="fiveday-more-info">more info</button>
+                </li>`
+      )
+    ).join('')}
+              </ul>
+  `;
+
+  let activeKey;
+  const activeLi = document.querySelector(`li.active`);
+  if (activeLi) {
+    activeKey = activeLi.dataset.key;
+  }
+
+  stopAnimation();
+  weatherWrapElement.innerHTML = ul;
+
+  data.forEach(d => {
+    const btn = document.querySelector(`button[data-key="${d.key}"]`);
+    btn.addEventListener('click', e => {
+      e.preventDefault();
+      renderMoreInfo(d.key, d.list);
+
+      startAnimation({ weather: [{ main: d.weatherName }] });
+    });
+  });
+
+  if (activeKey) {
+    renderMoreInfo(activeKey, data.find(d => d.key === activeKey).list);
+  }
+}
+
+function renderMoreInfo(key, list) {
+  const moreInfoDataElement = document.getElementById('more-info-data');
+  moreInfoDataElement.innerHTML = '';
+
+  if (list.length === 0) {
+    return;
+  }
+
+  const li = document.querySelector(`li[data-key="${key}"]`);
+  if (li.classList.contains('active')) {
+    li.classList.toggle('active');
+    return;
+  }
+
+  const activeLi = document.querySelector(`li.active`);
+  if (activeLi) {
+    activeLi.classList.toggle('active');
+  }
+
+  li.classList.add('active');
+
+  const ul = `<div class="more-info-container" data-aos="fade-down" data-aos-delay="100" data-aos-duration="600">
+      <ul class="more-info-list">
+        ${Array.from(
+          list.map(
+            item =>
+              `<li class="more-info-item">
+                <h3 class="time">${moment(item.dt_txt).format('HH:mm')}</h3>
+                <img src="${WEATHER_ICON_ENDPOINT}/img/wn/${
+                item.weather[0].icon
+              }.png" alt="" class="icon">
+                <h2 class="temperature">${Math.floor(item.main.temp)}°</h2>
+                    <ul class="measurement-list">
+                      <li class="measurement-item">
+                        <span class="icon">
+                          <svg width="20" height="20">
+                            <use href="/icons.adfc4680.svg#barometer"></use>
+                          </svg>
+                        </span>
+                        <span class="barometer">${item.main.pressure} mm</span>
+                      </li>
+                      <li class="measurement-item">
+                        <span class="icon">
+                          <svg width="20" height="20">
+                            <use href="/icons.adfc4680.svg#humidity"></use>
+                          </svg>
+                        </span>
+                        <span class="humidity">${item.main.humidity}%</span>
+                      </li>
+                      <li class="measurement-item">
+                        <span class="icon">
+                          <svg width="20" height="20">
+                            <use href="/icons.adfc4680.svg#wind"></use>
+                          </svg>
+                        </span>
+                        <span class="wind">${item.wind.speed} m/s</span>
+                      </li>
+                    </ul>
+              </li>`
+          )
+        ).join('')}
+            </ul></div>`;
+  moreInfoDataElement.innerHTML = ul;
+}
