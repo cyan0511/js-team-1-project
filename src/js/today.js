c31673b6f4300e7e7ffc134ef848f96d9a2a847e
Jummelmarijuan <142652721+Jummelmarijuan@users.noreply.github.com>
2024-05-29 13:17:15 +0900

    update css
    Update media queries for tablet/mobile and desktop view on index.html

--- src/js/today.js
+++ src/js/today.js
@@ -6,13 +6,13 @@ export function renderWeatherData(data) {
   ).textContent = `${data.name}, ${data.sys.country}`;
   document.querySelector('.current-temp').innerHTML = `${Math.round(
     data.main.temp
-  )}°<sup>c</sup>`;
+  )}°`;
   document.querySelector('.min-temp').innerHTML = `${Math.round(
     data.main.temp_min
-  )}°<sup>c</sup>`;
+  )}°`;
   document.querySelector('.max-temp').innerHTML = `${Math.round(
     data.main.temp_max
-  )}°<sup>c</sup>`;
+  )}°`;
 
   const weatherIconElement = document.querySelector('.weather-icon');
   const iconCode = data.weather[0].icon;
